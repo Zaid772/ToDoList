90d8101647b8d555122caaaca8374ec8668caa43
Zaid <zedpx101@outlook.com>
2023-09-01 12:11:08 +0100

    input bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+
+  const[inputText,setInputText] = useState("");
+
+  const changeHandler = (event) => {
+    setInputText(event.target.value)
+  }
+
   return (
     <div className="App">
-
+      <h1> My To do List!</h1>
+      <input type='text' onChange={changeHandler}></input>
     </div>
   );
 }
