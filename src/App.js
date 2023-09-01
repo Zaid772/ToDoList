a8cc2540cc54887963ec527b329dc701f298848d
Zaid <zedpx101@outlook.com>
2023-09-01 15:53:02 +0100

    Add button with function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import './App.css';
 import { useState } from 'react';
-function App() {
 
-  const[inputText,setInputText] = useState("");
+const App = () => {
 
-  const changeHandler = (event) => {
-    setInputText(event.target.value)
-  }
+  const [list, setlist] = useState([]);
+  const [inputText, setInputText] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleAddList = () => {
+    if (inputText.trim() !== '') {
+      setlist([...list, inputText]);
+      setInputText('');
+    }
+  };
 
   return (
-    <div className="App">
-      <h1> My To do List!</h1>
-      <input type='text' onChange={changeHandler}></input>
+    <div>
+      <h1>My To-Do List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task"
+          value={inputText}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddList}>Add...</button>
+      </div>
+      <ul>
+        {list.map((todo, index) => (
+          <li key={index}>{todo}</li>
+
+        ))}
+      </ul>
     </div>
   );
 }
